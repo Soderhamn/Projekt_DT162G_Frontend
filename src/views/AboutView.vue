202dafeacf854aee319a5e0b8117841600a35e05
Marcus Andersson <marcus@apparant.se>
2023-06-16 13:33:27 +0200

    Rensat lite

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit optio laborum voluptatem voluptatibus est veniam placeat totam, dolores repellendus cumque. Odio rerum minima, dolore tempora error dignissimos perferendis dicta rem?</p>
   </div>
 </template>
 
